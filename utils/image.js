28bc2d43e64d7efa75fcf2ee8abee755f5420c56
Tristan Lyons <tristan.n.lyons@gmail.com>
2024-05-16 04:07:24 -0400

    Continued work on image generation. Usercard improving.

--- utils/image.js
+++ utils/image.js
@@ -34,27 +34,43 @@ class Image {
 
     /*
     *   setBackground
-    *   Sets the background of the image.
+    *   Sets the background of the canvas.
     *   @PARAM {string} bgColor - the background color in hex format (#ffffff).
     *   @PARAM {string} bColor - color in hex form for border (optional).
     *   @PARAM {string} bThickness - thickness for border (default = 1).
     *   @RETURN - None.
     */
     async setBackground(bgColor, bColor = false, bThickness = 1) {
-        // Check bgColor for validity.
-        bgColor = bgColor.toLowerCase();
-        if (!bgColor.match(/^[#](\d|[a-f]){6}$/)) return console.error("Background color must be in the format #ffffff.");
+        // Color in background.
+        await this.drawRectangle(0, 0, this.canvas.width, this.canvas.height, bgColor);
+        // If border is specified...
+        if (bColor) await this.drawRectangle(0, 0, this.canvas.width, this.canvas.height, bColor, bThickness);
+    }
+
+    /*
+    *   drawRectangle
+    *   Draws a rectangle to the canvas.
+    *   @PARAM {integer} x - the x coordinate of the rectangle (top left corner).
+    *   @PARAM {integer} y - the x coordinate of the rectangle (top left corner).
+    *   @PARAM {integer} w - the width of the rectangle.
+    *   @PARAM {integer} h - the height of the rectangle.
+    *   @PARAM {string} color - the color of the rectangle.
+    *   @PARAM {integer} thickness - optional thickness for the rectangle. If not provided the rectangle will be filled in.
+    *   @RETURN - None.
+    */
+    async drawRectangle(x, y, w, h, color, thickness = false) {
         // Save the current context.
         this.context.save();
-        // Color in background.
-        this.context.fillStyle = bgColor;
-        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        // If border is specified...
-        if (bColor) {
-            if (!bColor.match(/^[#](\d|[a-f]){6}$/)) return console.error("Border color must be in the format #ffffff.");
-            this.context.strokeStyle = bColor;
-            this.context.lineWidth = bThickness;
-            this.context.strokeRect(0, 0, this.canvas.width, this.canvas.height);
+        // Draw the rectangle.
+        if (thickness) {
+            // Draw a rectangle outline.
+            this.context.strokeStyle = color;
+            this.context.lineWidth = thickness;
+            this.context.strokeRect(x, y, w, h);
+        } else {
+            // Draw a solid rectangle.
+            this.context.fillStyle = color;
+            this.context.fillRect(x, y, w, h);
         }
         // Restore the context from before starting.
         this.context.restore();
@@ -62,7 +78,7 @@ class Image {
 
     /*
     *   drawCircle
-    *   Draws a circle.
+    *   Draws a circle to the canvas.
     *   @PARAM {integer} x - the x coordinate of the center of the circle.
     *   @PARAM {integer} y - the y coordinate of the center of the circle.
     *   @PARAM {integer} radius - the radius of the circle in pixels.
@@ -71,9 +87,6 @@ class Image {
     *   @RETURN - None.
     */
     async drawCircle(x, y, radius, color, thickness = false) {
-        // Check color for validity.
-        color = color.toLowerCase();
-        if (!color.match(/^[#](\d|[a-f]){6}$/)) return console.error("Color must be in the format #ffffff.");
         // Save the current context.
         this.context.save();
         // Draw the circle.
@@ -160,6 +173,91 @@ class Image {
     }
 
     /*
+    *   sizeText
+    *   Sets this context to the appropriate font size for the text you want to draw.
+    *   @PARAM {string} text - the text that's going to be drawn.
+    *   @PARAM {integer} maxWidth - the maximum width your text should take up (in pixels).
+    *   @RETURN - None.
+    */
+    async sizeText(text, maxWidth) {
+        let fontSize = 100;
+        do {
+            this.context.font = `${fontSize -= (fontSize <= 20) ? 3 : 10}px sans-serif`;
+        } while (this.context.measureText(text).width > maxWidth);
+    }
+
+    /*
+    *   drawText
+    *   Draws text on the canvas.
+    *   @PARAM {string} text - the text to draw.
+    *   @PARAM {integer} x - x coordinate to place text (bottom left corner).
+    *   @PARAM {integer} y - y coordinate to place text (bottom left corner).
+    *   @PARAM {integer} maxWidth - the maximum width the text can take up.
+    *   @PARAM {string} color - the color of the text (default white).
+    */
+    async drawText(text, x, y, maxWidth, color = "#ffffff") {
+        // Save the current context.
+        this.context.save();
+        // Set the text color and size within the context.
+        await this.sizeText(text, maxWidth);
+        this.context.fillStyle = color;
+        // Draw the text.
+        this.context.fillText(text, x, y);
+        // Restore the context from before starting.
+        this.context.restore();
+    }
+
+    /*
+    *   drawLoadingBar
+    *   Draws a loading/progress/data bar on the canvas.
+    *   @PARAM {integer} x - the x coordinate of the loading bar (top left corner).
+    *   @PARAM {integer} y - the y coordinate of the loading bar (top left corner).
+    *   @PARAM {integer} maxWidth - the maximum width of the loading bar (may be scaled down slightly to fit data nicely).
+    *   @PARAM {integer} height - the height of the loading bar.
+    *   @PARAM {array} colorData - array of hex codes for the colors to be drawn in each segment.
+    *   @PARAM {integer} subSegments - an optional number representing the number of sub-segments per larger segment there should be.
+    *                                  If this option is provided a value the colorData will be split into n/subSegments larger segments,
+    *                                  with subSegments sub-segments to each larger segment. If too much color data is provided the beginning 
+    *                                  will be truncated.
+    *   @PARAM {string} borderColor - the color of the border lines on the loading bar (default black).
+    *   @RETURN - None.
+    */
+    async drawLoadingBar(x, y, maxWidth, height, colorData, subSegments = false, borderColor = "#000000") {
+        const borderThickness = 7;
+        const subBorderThickness = 2;
+        // Remove from the overall width any extra length that won't be needed.
+        var width = maxWidth - ((maxWidth - borderThickness) % colorData.length); // -5 accounts for there being 1 more outline border edge than segment.
+        // Draw the background rectangle.
+        await this.drawRectangle(x, y, width, height, borderColor);
+        // If subsegments are enabled:
+        if (subSegments) {
+            // Trim data off the beginning if necessary so the segments can be drawn evenly.
+            var trimAmount = colorData.length % subSegments;
+            colorData = colorData.slice(trimAmount);
+            // Draw segments.
+            var segmentWidth = colorData.length / subSegments;
+            for (i = 0; i < colorData.length; i++) {
+                let subSegmentThickness = ((width - ((((subSegments * segmentWidth) - 1) * subBorderThickness) + (segmentWidth * borderThickness))) / colorData.length);
+                let curSegment = Math.floor(i / segmentWidth); // How many segments (not sub-segments) have been drawn.
+                let tempY = y + borderThickness;
+                let tempX = (x + borderThickness) + // Shift for initial thick border (left outer boarder).
+                            (curSegment * borderThickness) + // Shift for current large inner borders (segment border).
+                            ((i - curSegment) * subBorderThickness) + // Shift for subsegment borders.
+                            (i * subSegmentThickness); // Shift for segment fill-in.
+                await this.drawRectangle(tempX, tempY, subSegmentThickness, (height - (borderThickness * 2)), colorData[i]);
+
+            }
+        // Otherwise just draw each segment.
+        } else {
+            for (i = 0; i < colorData.length; i++) {
+                let tempY = y + borderThickness;
+                let tempX = (x + borderThickness) + (i * ((width - borderThickness) / colorData.length));
+                await this.drawRectangle(tempX, tempY, (((width - borderThickness) / colorData.length) - borderThickness), (height - (borderThickness * 2)), colorData[i]);
+            }
+        }
+    }
+
+    /*
     *   getAttachment
     *   Builds a Discord attachment from the current canvas.
     *   @RETURN {AttachmentBuilder} - returns an attachment of the current image.
@@ -203,6 +301,25 @@ class UserCard extends Image {
         } else {
             await super.drawCircleImage(this.member.user.displayAvatarURL(), 25, 25);
         }
+        // Draw the header text.
+        await super.drawText("User Activity (24hr):", 180, 55, 400);
+        // Draw username.
+        await super.drawText(this.member.displayName, 180, 135, 720);
+        // Draw user ID.
+        await super.drawText(`id:${this.member.id}`, 590, 40, 200);
+        // Draw the activity bar.
+        await super.drawLoadingBar(20, 180, 760, 80, [COLORS.status.online, COLORS.status.idle, COLORS.status.dnd, COLORS.status.idle, COLORS.status.online,
+                                                      COLORS.status.dnd, COLORS.status.online, COLORS.status.idle, COLORS.status.online, COLORS.status.dnd,
+                                                      COLORS.status.idle, COLORS.status.idle, COLORS.status.dnd, COLORS.status.idle, COLORS.status.online,
+                                                      COLORS.status.dnd, COLORS.status.dnd, COLORS.status.dnd, COLORS.status.online, COLORS.status.online,
+                                                      COLORS.status.online, COLORS.status.idle, COLORS.status.dnd, COLORS.status.idle, COLORS.status.online,
+                                                      COLORS.status.dnd, COLORS.status.online, COLORS.status.idle, COLORS.status.online, COLORS.status.dnd,
+                                                      COLORS.status.idle, COLORS.status.idle, COLORS.status.dnd, COLORS.status.idle, COLORS.status.online,
+                                                      COLORS.status.dnd, COLORS.status.dnd, COLORS.status.dnd, COLORS.status.online, COLORS.status.online,
+                                                      COLORS.status.online, COLORS.status.idle, COLORS.status.dnd, COLORS.status.idle, COLORS.status.online,
+                                                      COLORS.status.dnd, COLORS.status.online, COLORS.status.idle, COLORS.status.online, COLORS.status.dnd,
+                                                      COLORS.status.idle, COLORS.status.idle, COLORS.status.dnd, COLORS.status.idle, COLORS.status.online,
+                                                      COLORS.status.dnd, COLORS.status.dnd, COLORS.status.dnd, COLORS.status.online, COLORS.status.online], 5);
     }
 }
 
